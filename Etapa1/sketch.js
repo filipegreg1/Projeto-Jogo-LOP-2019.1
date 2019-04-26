73c0af11c5f59597ae546a9acee799dbca9c13eb
Filipe Gregorio <50002240+filipegreg1@users.noreply.github.com>
2019-04-26 00:36:52 -0300

    Add files via upload
    Etapa 1 jogo

---
+++ Etapa1/sketch.js
@@ -0,0 +1,24 @@
+/* 
+    Equipe: 
+        Filipe Gregório de Farias e Silva - Subturma D (Líder) 
+        Ivamar Dantas Roque - Subturma D 
+        Etapa 2
+        Jogo em Estilo Plataforma
+*/
+var x = 95
+
+function setup() {
+  createCanvas(800, 600);
+}
+
+function draw() {
+ background(227,218,205)
+
+  fill(145,87,35);
+  rect (650,485,50,68);
+  fill (255);
+  ellipse (x,535,38,38);
+  fill(0);
+  ellipse(x, 535, 30, 30);
+
+}
